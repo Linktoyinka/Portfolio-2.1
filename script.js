017dc79a97aaa177f5ce14289b6b62e67da14fef
linktoyinka <linktoyinka@gmail.com>
2023-03-14 10:52:56 +0100

    projects added

--- script.js
+++ script.js
@@ -37,3 +37,19 @@ nav3.addEventListener('click', function(){
     main_menu.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
 })
+
+
+let loadMoreBtn = document.querySelector('#load-more');
+let currentItem = 3;
+
+loadMoreBtn.onclick = () =>{
+   let boxes = [...document.querySelectorAll(".opp-card")];
+   for (var i = currentItem; i < currentItem + 3; i++){
+      boxes[i].style.display = 'inline-block';
+   }
+   currentItem += 3;
+
+   if(currentItem >= boxes.length){
+      loadMoreBtn.style.display = 'none';
+   }
+}
