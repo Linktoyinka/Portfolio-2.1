869bfcdd7dd8c06abadc2dc8e21d1cd6474b1cdf
linktoyinka <linktoyinka@gmail.com>
2023-03-14 01:31:22 +0100

    menu fixed

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ window.addEventListener("load", vanish);
 function vanish() {
   loader.classList.add("disppear");
 }
+
+
+const nav = document.querySelector('.nav-link')
+
+nav.addEventListener('click', function(){
+    main_menu.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+})
+
+
+const nav2 = document.querySelector('.nav-link2')
+
+nav2.addEventListener('click', function(){
+    main_menu.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+})
+
+
+const nav3 = document.querySelector('.nav-link3')
+
+nav3.addEventListener('click', function(){
+    main_menu.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+})
