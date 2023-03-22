01214d14fbdd1a3aa2950aa04c7160702737ec52
linktoyinka <linktoyinka@gmail.com>
2023-03-22 16:32:17 +0100

    documents formatted

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const main_menu = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.nav-mobile');
+const main_menu = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".nav-mobile");
 
-main_menu.addEventListener('click', function(){
-    main_menu.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
+main_menu.addEventListener("click", function () {
+  main_menu.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
 });
 
-const loader = document.querySelector(".loader")
+const loader = document.querySelector(".loader");
 
 window.addEventListener("load", vanish);
 
@@ -14,42 +14,38 @@ function vanish() {
   loader.classList.add("disppear");
 }
 
+const nav = document.querySelector(".nav-link");
 
-const nav = document.querySelector('.nav-link')
+nav.addEventListener("click", function () {
+  main_menu.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
+});
 
-nav.addEventListener('click', function(){
-    main_menu.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
-})
+const nav2 = document.querySelector(".nav-link2");
 
+nav2.addEventListener("click", function () {
+  main_menu.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
+});
 
-const nav2 = document.querySelector('.nav-link2')
+const nav3 = document.querySelector(".nav-link3");
 
-nav2.addEventListener('click', function(){
-    main_menu.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
-})
+nav3.addEventListener("click", function () {
+  main_menu.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
+});
 
-
-const nav3 = document.querySelector('.nav-link3')
-
-nav3.addEventListener('click', function(){
-    main_menu.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
-})
-
-
-let loadMoreBtn = document.querySelector('#load-more');
+let loadMoreBtn = document.querySelector("#load-more");
 let currentItem = 3;
 
-loadMoreBtn.onclick = () =>{
-   let boxes = [...document.querySelectorAll(".opp-card")];
-   for (var i = currentItem; i < currentItem + 3; i++){
-      boxes[i].style.display = 'inline-block';
-   }
-   currentItem += 3;
+loadMoreBtn.onclick = () => {
+  let boxes = [...document.querySelectorAll(".opp-card")];
+  for (var i = currentItem; i < currentItem + 3; i++) {
+    boxes[i].style.display = "inline-block";
+  }
+  currentItem += 3;
 
-   if(currentItem >= boxes.length){
-      loadMoreBtn.style.display = 'none';
-   }
-}
+  if (currentItem >= boxes.length) {
+    loadMoreBtn.style.display = "none";
+  }
+};
